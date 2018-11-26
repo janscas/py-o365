3b085526d550e321a406764033a814b8e7d92799
Jandro <janscas@users.noreply.github.com>
2018-11-26 12:08:44 +0100

    Fixed Missing __version__.py and not version.json required anymore on release.py

--- release.py
+++ release.py
@@ -6,7 +6,6 @@ import os
 import shutil
 import subprocess
 import sys
-import json
 import requests
 from pathlib import Path
 
@@ -18,26 +17,7 @@ PYPI_PACKAGE_NAME = 'pyo365'
 PYPI_URL = 'https://pypi.org/pypi/{package}/json'
 DIST_PATH = 'dist'
 DIST_PATH_DELETE = 'dist_delete'
-VERSION_FILE_NAME = 'version.json'
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
-
-
-def build_version_file(provided_version):
-    """ Updates the version.json file with the current version to be released """
-    version_json = Path(VERSION_FILE_NAME)
-
-    if provided_version == 'auto':
-        # Read version.json
-        with version_json.open('r') as version_file:
-            version = json.load(version_file)
-        current_version = version.get('version')
-
-        version_parts = [int(part) for part in current_version.split('.')]
-        version_parts[-1] += 1  # auto increment last version part. Major + Minor versions must be set manually
-        provided_version = '.'.join(str(part) for part in version_parts)
-
-    with version_json.open('w') as version_file:
-        json.dump({'version': provided_version}, version_file)
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
@@ -47,8 +27,7 @@ def cli():
 
 @cli.command()
 @click.option('--force/--no-force', default=False, help='Will force a new build removing the previous ones')
-@click.option('--version', type=str, default='auto', help="Provide a valid version number or autoincrement with 'auto'")
-def build(force, version):
+def build(force):
     """ Builds the distribution files: wheels and source. """
     dist_path = Path(DIST_PATH)
     if dist_path.exists() and list(dist_path.glob('*')):
@@ -60,9 +39,6 @@ def build(force, version):
             click.echo('Aborting')
             sys.exit(1)
 
-    if version:
-        build_version_file(version)
-
     subprocess.check_call(['python', 'setup.py', 'bdist_wheel'])
     subprocess.check_call(['python', 'setup.py', 'sdist',
                            '--formats=gztar'])
@@ -71,9 +47,8 @@ def build(force, version):
 @cli.command()
 @click.option('--release/--no-release', default=False, help='--release to upload to pypi otherwise upload to test.pypi')
 @click.option('--rebuild/--no-rebuild', default=True, help='Will force a rebuild of the build files (src and wheels)')
-@click.option('--version', type=str, default='auto', help="Provide a valid version number or autoincrement with 'auto'")
 @click.pass_context
-def upload(ctx, release, rebuild, version):
+def upload(ctx, release, rebuild):
     """ Uploads distribuition files to pypi or pypitest. """
 
     dist_path = Path(DIST_PATH)
@@ -82,7 +57,7 @@ def upload(ctx, release, rebuild, version):
             print("No distribution files found. Please run 'build' command first")
             return
     else:
-        ctx.invoke(build, force=True, version=version)
+        ctx.invoke(build, force=True)
 
     if release:
         args = ['twine', 'upload', 'dist/*']
--- setup.py
+++ setup.py
@@ -1,23 +1,11 @@
 import os
-import json
-from pathlib import Path
 from setuptools import setup, find_packages
-
-
-VERSION_FILE_NAME = 'version.json'
 
 
 def read(fname):
     """ Returns the contents of the fname file """
     with open(os.path.join(os.path.dirname(__file__), fname), 'r') as file:
         return file.read()
-
-
-def get_version():
-    """ Reads the version from 'version.json' file. Raises an error if not found. """
-    with Path(VERSION_FILE_NAME).open('r') as version_file:
-        version = json.load(version_file)
-        return version.get('version')
 
 
 # Available classifiers: https://pypi.org/pypi?%3Aaction=list_classifiers
@@ -50,7 +38,7 @@ requires = [
 
 setup(
     name='pyo365',
-    version=get_version(),
+    version='0.1.3',
     packages=find_packages(),
     url=' https://github.com/janscas/pyo365',
     license='Apache License 2.0',
